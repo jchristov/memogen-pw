8002417c7ed04fb2c0840085b90f25a05e658457
Yuliyan HRISTOV <WZHHVY@rbinternational.com>
2024-11-13 20:16:31 +0100

    Refactoring, logo

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -1,62 +1,84 @@
 <template>
-  <v-container fluid class="mb-6 fill-height">
+  <v-container>
+    <v-row align="center" class="mt-8">
+      <v-col class="text-center">
+        <v-img src="@/assets/logo.png" height="64"></v-img>
+        <h1> <span class="text-primary">Memogen.pw</span> Secure Password Generator </h1>
+        <h4> Generate strong, unique and memorable passwords </h4>
+      </v-col>
+      <v-btn v-if="0" variant="text" @click="toggleTheme()" class="position-absolute right-0" :icon="themeIcon" />
+      <v-btn v-if="0" variant="text" size="small" class="position-absolute right-0" @click="toggleTheme()"
+        :prepend-icon="themeIcon" :text="themeName + ' mode'" />
+    </v-row>
+
+    <v-row align="center" justify="space-between">
+      <v-col>
+        <v-btn-toggle v-model="passwordSettings.type" class="ma-2 bg-surface" color="primary" density="compact"
+          mandatory rounded="xl">
+          <v-btn size="small" rounded="xl" variant="elevated">Random</v-btn>
+          <v-btn size="small" rounded="xl" variant="elevated">Memorable</v-btn>
+        </v-btn-toggle>
+      </v-col>
+      <v-col align="end">
+        <v-btn variant="text" @click="toggleTheme()" :prepend-icon="themeIcon" :text="themeName + ' mode'" />
+      </v-col>
+    </v-row>
+
     <v-row>
-      <v-col align-self="center">
-        <v-card min-height="500" elevation="2" rounded="xl" color="surface-light" class="pa-8">
-          <div class="pb-4">
-            <h1 class="text-center"><span class="text-primary">Memogen.pw</span> Secure Password Generator</h1>
-            <h4 class="text-center">Generate strong, unique and memorable passwords</h4>
-          </div>
-          <v-btn v-if="0" @click="toggleTheme()" class="float-end">Toggle theme</v-btn>
-          <v-row>
-            <v-col cols="auto">
-              <div>
-                <v-btn-toggle v-model="passwordSettings.type" class="ma-2 bg-surface" color="primary" density="compact"
-                  mandatory shaped rounded="xl">
-                  <v-btn size="small" rounded="xl" variant="elevated">Random</v-btn>
-                  <v-btn size="small" rounded="xl" variant="elevated">Memorable</v-btn>
-                </v-btn-toggle>
-                <div class="d-flex align-center">
-                  <v-checkbox v-model="passwordSettings.includeLowercase" label="Include Lowercase" class="ma-2"
-                    color="primary" hide-details readonly />
-                  <span class="ml-auto font-weight-black text-h5" :class="getColorClass('a')">abc</span>
-                </div>
-                <div class="d-flex align-center">
-                  <v-checkbox v-model="passwordSettings.includeUppercase" label="Include Uppercase" class="ma-2"
-                    color="primary" hide-details @change="passwordsGenerated && generatePasswords()" />
-                  <span class="ml-auto font-weight-black text-h5" :class="getColorClass('A')">ABC</span>
-                </div>
-                <div class="d-flex align-center">
-                  <v-checkbox v-model="passwordSettings.includeNumbers" label="Include Numbers" class="ma-2"
-                    color="primary" hide-details @change="passwordsGenerated && generatePasswords()" />
-                  <span class="ml-auto font-weight-black text-h5" :class="getColorClass('1')">123</span>
-                </div>
-                <div class="d-flex align-center">
-                  <v-checkbox v-model="passwordSettings.includeSymbols" label="Include Symbols" class="ma-2"
-                    color="primary" hide-details @change="passwordsGenerated && generatePasswords()" />
-                  <span class="ml-auto font-weight-black text-h5" :class="getColorClass('.')">./-</span>
-                </div>
-                <div class="d-flex align-center">
-                  <v-select v-model="passwordSettings.separator" class="ma-2" color="primary" label="Separator"
-                    :items="separators" variant="solo-filled" hide-details
-                    @update:modelValue="passwordsGenerated && generatePasswords()" />
-                </div>
-              </div>
-            </v-col>
-            <v-col>
-              <v-sheet color="surface" class="pa-8" rounded="xl" height="100%">
-                <div v-for=" (password, pIdx) in generatedPasswords" :key="pIdx" class="password">
-                  <v-btn icon="mdi-content-copy" variant="text" @click="copyPassword(password)" v-if="isSupported" />
-                  <span v-for="(char, cIdx) in password" :key="cIdx" :class="getColorClass(char)" v-html="char" />
-                </div>
-              </v-sheet>
-            </v-col>
-          </v-row>
-          <div class="mt-4 text-center">
-            <v-btn block color="primary" size="large" prepend-icon="mdi-lock-reset" @click="generatePasswords">Generate
-              Passwords</v-btn>
+      <v-col cols="12">
+        <v-card color="surface-light" class="pa-4" min-height="80" rounded="lg">
+          <div v-for=" (password, pIdx) in generatedPasswords" :key="pIdx" class="password">
+            <v-btn icon="mdi-content-copy" variant="text" @click="copyPassword(password)" v-if="isSupported" />
+            <span v-for="(char, cIdx) in password" :key="cIdx" :class="getColorClass(char)" v-html="char" />
           </div>
         </v-card>
+        <div class="mt-4 text-center">
+          <v-btn block color="primary" size="large" prepend-icon="mdi-autorenew" @click="generatePasswords(1)" text="Generate
+            New" />
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row align="center">
+      <v-col cols="12" md="6">
+        <span class="text-h5">Words: {{ passwordSettings.words }}</span>
+        <v-slider v-if="1" v-model="passwordSettings.words" color="primary" dense hide-details :min="1" :max="10"
+          :step="1" :thumb-size="20" thumb-label @update:modelValue="generatePasswords()" />
+        <v-number-input v-if="0" v-model="passwordSettings.words" control-variant="split" :min="1" :max="10"
+          variant="solo" dense />
+      </v-col>
+      <v-col cols="12" md="6">
+        <span class="text-h5">Include</span>
+        <div class="d-flex flex-wrap">
+          <div class="d-flex align-center">
+            <v-checkbox v-model="passwordSettings.includeLowercase" label="Lowercase" hide-details readonly />
+            <span class="font-weight-black text-h6 ml-2" :class="getColorClass('a')">abc</span>
+          </div>
+          <div class="d-flex align-center">
+            <v-checkbox v-model="passwordSettings.includeUppercase" label="Uppercase" hide-details
+              @change="passwordsGenerated && generatePasswords()" />
+            <div class="font-weight-black text-h6 ml-2" :class="getColorClass('A')">ABC</div>
+          </div>
+          <div class="d-flex align-center">
+            <v-checkbox v-model="passwordSettings.includeNumbers" label="Numbers" hide-details
+              @change="passwordsGenerated && generatePasswords()" />
+            <span class="font-weight-black text-h6 ml-2" :class="getColorClass('1')">123</span>
+          </div>
+          <div class="d-flex align-center">
+            <v-checkbox v-model="passwordSettings.includeSymbols" label="Symbols" hide-details
+              @change="passwordsGenerated && generatePasswords()" />
+            <span class="font-weight-black text-h6" :class="getColorClass('.')">./-</span>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row align="center">
+      <v-col cols="12">
+        <div class="d-flex align-center">
+          <v-select v-model="passwordSettings.separator" class="ma-2" color="primary" label="Separator"
+            :items="separators" variant="solo-filled" hide-details
+            @update:modelValue="passwordsGenerated && generatePasswords()" />
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -73,26 +95,32 @@
 
 <script setup>
 import { useClipboard, useDark, usePermission, useToggle } from '@vueuse/core';
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { useTheme } from 'vuetify';
+import { VNumberInput } from 'vuetify/labs/VNumberInput';
 import dictionary from './dictionary';
 
+// Clipboard support
 const { text, isSupported, copy } = useClipboard()
 const permissionRead = usePermission('clipboard-read')
 const permissionWrite = usePermission('clipboard-write')
 
-const isDark = useDark({ selector: '.v-application' })
+// Dark mode support
+const isDark = useDark()
 const toggleDark = useToggle(isDark)
-
 const theme = useTheme()
+const themeName = computed(() => isDark.value ? 'light' : 'dark')
+const themeIcon = computed(() => isDark.value ? 'mdi-brightness-7' : 'mdi-brightness-4')
 
 function toggleTheme() {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  toggleDark()
+  theme.global.name.value = isDark.value ? 'dark' : 'light'
 }
 
 const separators = [{ title: 'None', value: '' }, { title: 'Space', value: ' ' }, { title: 'Dash', value: '-' }, { title: 'Underscore', value: '_' }, { title: 'Period', value: '.' }, { title: 'Comma', value: ',' }]
 
 const passwordSettings = reactive({
+  words: 5,
   length: 12,
   type: 1,
   separator: ' ',
@@ -106,7 +134,6 @@ const snackbar = ref(false);
 const snackbarMessage = ref('');
 const generatedPasswords = ref([]);
 const passwordsGenerated = ref(false)
-// const generatedPasswords = computed(() => generatePasswords())
 
 function getColor(char) {
   if (/[a-z]/.test(char)) return 'yellow';
@@ -141,17 +168,19 @@ function generatePassword() {
   return words.join(passwordSettings.separator);
 }
 
-function generatePasswords() {
-  const passwordCount = 5;
+function generatePasswords(count = 1) {
+  const passwordCount = count;
   const passwords = [];
+  console.log('Generating password', count);
   for (let i = 0; i < passwordCount; i++) {
     const password = generatePassword()
     passwords.push(password);
+    console.log('Password', password);
   }
-  // console.log(passwords);
+  console.log(passwords);
   generatedPasswords.value = passwords;
-  snackbarMessage.value = 'Passwords successfully generated';
-  snackbar.value = true;
+  // snackbarMessage.value = 'Passwords successfully generated';
+  // snackbar.value = true;
   passwordsGenerated.value = true;
 }
 
@@ -160,6 +189,8 @@ function copyPassword(password) {
   snackbarMessage.value = 'Password copied to clipboard';
   snackbar.value = true;
 }
+
+generatePasswords(1);
 </script>
 
 <style>
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,16 @@
 // Plugins
-import Components from 'unplugin-vue-components/vite'
 import Vue from '@vitejs/plugin-vue'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import ViteFonts from 'unplugin-fonts/vite'
+import Components from 'unplugin-vue-components/vite'
+import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // Utilities
-import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'memogen-pw',
+  base: '/memogen-pw',
   plugins: [
     Vue({
       template: { transformAssetUrls }
