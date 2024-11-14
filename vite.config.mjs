ce9a3b4dfa0962abfaa5b83a21e4c27088872c1d
Julian Christov <julian.christov@gmail.com>
2024-11-14 21:48:27 +0100

    Removed base

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/memogen-pw',
+  // base: '/memogen-pw',
   plugins: [
     Vue({
       template: { transformAssetUrls }
